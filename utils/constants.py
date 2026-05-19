ddfc47e05f7101b91821a1e9a6c3f05a9552fa4b
zeeinz-ux <zeezeeinz@gmail.com>
2026-05-20 03:05:56 +0700

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,32 @@
 LAVALINK_NODES = [
+    # Node 1: HeavenCloud (Global, 10Gbps)
+    {
+        "uri": "https://89.106.84.59:4000",
+        "password": "heavencloud.in",
+    },
+    # Node 2: Jirayu (Global)
+    {
+        "uri": "https://lavalink.jirayu.net:13592",
+        "password": "youshallnotpass",
+    },
+    # Node 3: G3V UK (Europe)
+    {
+        "uri": "https://lava.g3v.co.uk:9008",
+        "password": "lavalinklol",
+    },
+    # Node 4: NyxBot SG (Singapore, YouTube plugin)
+    {
+        "uri": "https://sg1-nodelink.nyxbot.app:3000",
+        "password": "nyxbot.app/support",
+    },
+    # Node 5: TriniumHost (Global)
+    {
+        "uri": "https://lavalink.triniumhost.com:4333",
+        "password": "free",
+    },
+    # Backup: AjieDev (kalau YouTube fix)
     {
         "uri": "https://lava-v4.ajieblogs.eu.org:443",
         "password": "https://dsc.gg/ajidevserver",
-    }
+    },
 ]
