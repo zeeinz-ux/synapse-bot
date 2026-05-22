e57ee050f7eb336a77aa3a3f4d1491537e13b88e
zeeinz-ux <zeezeeinz@gmail.com>
2026-05-22 12:24:04 +0700

    Update constants.py

--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -1,20 +1,21 @@
 LAVALINK_NODES = [
-    # Node 1: NyxBot SG (Singapore) - PRIORITY & FIXED
+    # Node 1: NyxBot SG (Singapore) - PRIORITY 1 & VERIFIED
     {
         "uri": "http://sg1-nodelink.nyxbot.app:3000",
         "password": "nyxbot.app/support",
     },
-    # Node 2: Jirayu (Global) - Backup 1
+    # Node 2: Jirayu (Global) - PRIORITY 2 & VERIFIED
     {
-        "uri": "https://lavalink.jirayu.net:13592",
+        "uri": "http://lavalink.jirayu.net:13592",
         "password": "youshallnotpass",
     },
-    # Node 3: G3V UK (Europe) - Backup 2
+    # --- Cadangan Lainnya ---
+    # Node 3: G3V UK (Europe)
     {
         "uri": "https://lava.g3v.co.uk:9008",
         "password": "lavalinklol",
     },
-    # Node 4: TriniumHost (Global) - Backup 3
+    # Node 4: TriniumHost (Global)
     {
         "uri": "https://lavalink.triniumhost.com:4333",
         "password": "free",
