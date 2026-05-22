2bb3e381ff7195d2d7a01c84a3b26f03629c76bb
zeeinz-ux <zeezeeinz@gmail.com>
2026-05-22 13:26:34 +0700

    Update constants.py

--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -1,32 +1,17 @@
 LAVALINK_NODES = [
-    # Node 1: ajieblogs (Terbukti berfungsi dari tes lokal Anda)
+    # Node 1: ajieblogs (Prioritas utama, terbukti berfungsi dari tes lokal Anda)
     {
         "uri": "https://lava-v4.ajieblogs.eu.org:443",
         "password": "https://dsc.gg/ajieblogs",
     },
-    # Node 2: Cadangan umum yang andal
+    # Node 2: Serenetia (Cadangan SSL, format sudah dikonversi dengan benar)
+    {
+        "uri": "https://lavalinkv4.serenetia.com:443",
+        "password": "https://seretia.link/discord",
+    },
+    # Node 3: Cadangan umum lainnya yang andal
     {
         "uri": "http://lava.link:80",
         "password": "youshallnotpass",
     },
-    # Node 3: Cadangan umum lainnya
-    {
-        "uri": "http://lavalink-coders.ml:80",
-        "password": "Lavalink",
-    },
-    # Node 4: v4(SSL)
-    {
-    "host": "lavalinkv4.serenetia.com",
-    "port": 443,
-    "password": "https://seretia.link/discord",
-    "secure": true
-    },
-    # Node 5: V4(NON SSL)
-    {
-    "host": "lavalinkv4.serenetia.com",
-    "port": 443,
-    "password": "https://seretia.link/discord",
-    "secure": true
-    },
-    
 ]
