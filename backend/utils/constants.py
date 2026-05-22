448104cd451dec91198075d37bc43de5bb1aeb79
zeeinz-ux <zeezeeinz@gmail.com>
2026-05-22 12:33:14 +0700

    Update constants.py

--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -1,32 +1,17 @@
 LAVALINK_NODES = [
-    # Node 1: NyxBot SG (Singapore) - PRIORITY 1 & VERIFIED
+    # Node 1: Singapura (NyxBot) - Telah diverifikasi, prioritas utama untuk latensi rendah.
     {
         "uri": "http://sg1-nodelink.nyxbot.app:3000",
         "password": "nyxbot.app/support",
     },
-    # Node 2: Jirayu (Global) - PRIORITY 2 & VERIFIED
+    # Node 2: Jirayu (Global) - Telah diverifikasi, cadangan global yang solid.
     {
         "uri": "http://lavalink.jirayu.net:13592",
         "password": "youshallnotpass",
     },
-    # --- Cadangan Lainnya ---
-    # Node 3: G3V UK (Europe)
+    # Node 3: TriniumHost (Global) - Telah diverifikasi, cadangan global yang solid.
     {
-        "uri": "https://lava.g3v.co.uk:9008",
-        "password": "lavalinklol",
-    },
-    # Node 4: TriniumHost (Global)
-    {
-        "uri": "https://lavalink.triniumhost.com:4333",
+        "uri": "http://lavalink.triniumhost.com:4333",
         "password": "free",
-    },
-    # --- Node Kurang Stabil atau Offline ---
-    {
-        "uri": "https://lava-v4.ajieblogs.eu.org:443",
-        "password": "https://dsc.gg/ajidevserver",
-    },
-    {
-        "uri": "https://89.106.84.59:4000",
-        "password": "heavencloud.in",
     },
 ]
